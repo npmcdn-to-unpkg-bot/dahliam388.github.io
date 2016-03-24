d6d7915fc3e23937c16d719d9cec70186344d5db
dahliam388 <dahliam@live.com>
2016-03-23 20:50:25 -0400

    got it!

--- Class-work/first-javascript/script.js
+++ Class-work/first-javascript/script.js
@@ -1,8 +1,13 @@
 alert('this is an alert. this is annoying');
 
 	document.write('writing to the document, like a boss');
-	document.write(10*10);
-	document.write(100/10);
-	document.write(50-17);
-	document.write(50+17);
-	document.write('Dahlia');
+
+	var num1 = 10*10;
+	var num2 = 100/10;
+	var num3 = 20/5;
+	var myName = 'Dahlia';
+	
+	document.write(num1, num2, num3, myName);
+
+	document.getElementById('title').style.color='red';
+	document.getElementById('title').style.background='blue';
