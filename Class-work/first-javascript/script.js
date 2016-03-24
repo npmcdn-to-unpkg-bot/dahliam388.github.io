e1f5e9ac0dad5dc03d2ca7bb3fb612ff5a32ed70
dahliam388 <dahliam@live.com>
2016-03-23 20:05:05 -0400

    yay

--- Class-work/first-javascript/script.js
+++ Class-work/first-javascript/script.js
@@ -1,3 +1,8 @@
 alert('this is an alert. this is annoying');
 
 	document.write('writing to the document, like a boss');
+	document.write(10*10);
+	document.write(100/10);
+	document.write(50-17);
+	document.write(50+17);
+	document.write('Dahlia');
