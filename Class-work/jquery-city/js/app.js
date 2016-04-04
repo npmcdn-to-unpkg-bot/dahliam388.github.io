aa3ce300b056a379abc4bbb5ef9d3ff2c511f394
dahliam388 <dahliam@live.com>
2016-04-04 19:57:49 -0400

    got attr

--- Class-work/jquery-city/js/app.js
+++ Class-work/jquery-city/js/app.js
@@ -1,2 +1,20 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
+$('#first').click(function () {
+	$('#bigimage').attr('src', 'img/1.jpg')
+})
+
+$('#second').click(function () {
+	$('#bigimage').attr('src', 'img/2.jpg')
+})
+
+$('#third').click(function () {
+	$('#bigimage').attr('src', 'img/3.jpg')
+})
+
+$('#fourth').click(function () {
+	$('#bigimage').attr('src', 'img/4.jpg')
+})
+
+
+//replace .click with .mouseover for the change to take place by hover instead of by click.
