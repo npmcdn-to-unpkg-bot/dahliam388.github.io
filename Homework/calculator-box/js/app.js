03827c6cc3dcfe527f9be4fbcdc6180b8b920755
dahliam388 <dahliam@live.com>
2016-04-04 18:22:57 -0400

    new class

--- Homework/calculator-box/js/app.js
+++ Homework/calculator-box/js/app.js
@@ -1,5 +1,7 @@
-// 1. Besure to write your code within the $(document).ready(...)
 $(document).ready(function(){
+	
+	var total = 0;
+
 // 2. Add .click() event handlers for each of the boxes:
 // - A) boxes #a10, #a20, and #a30 should each add 10, 20, and 30, respectively, to the value in the center box, #out
 // - B) boxes #n10, #n20, and #n30 should each subtract 10, 20, and 30, respectively, from the value in the center box, #out
@@ -7,8 +9,4 @@ $(document).ready(function(){
 // - D) #out should change the background of #out to white, and set the value back to zero
 // HINT: define a variable, var count = 0, at the top of your function; use this variable to keep track of what to display inside of #out
 // Ex: when the user clicks #a10, add 10 to the var count, and then reflect this change in the HTML
-//
-// jQuery Methods Needed:
-// - .click()
-// - . text() or .html()
-// - .css()
+
