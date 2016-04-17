3d59b3243c9c2e8367f3aba97dc3f03e63b311d6
dahliam388 <dahliam@live.com>
2016-04-17 19:37:50 -0400

    hey  now

--- Final Project/app.js
+++ Final Project/app.js
@@ -4,4 +4,54 @@ $(document).ready(function() {
       // When the user clicks the marker, an info window opens.
       // The maximum width of the info window is set to 200 pixels.
 
-      
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: {lat: 0, lng: -180},
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+
+  var flightPlanCoordinates = [
+    {lat: 37.772, lng: -122.214},
+    {lat: 21.291, lng: -157.821},
+    {lat: -18.142, lng: 178.431},
+    {lat: -27.467, lng: 153.027}
+  ];
+  var flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  flightPath.setMap(map);
+}
+
+var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">London, England</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>London</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '</div>'+
+            '</div>';
+
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+          position: England,
+          map: map,
+          title: 'England'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+      }
+
+})
