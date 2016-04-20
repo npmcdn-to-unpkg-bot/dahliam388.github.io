ba10b79ee60b6e9d6302c5b3210bdc2368b26e7e
dahliam388 <dahliam@live.com>
2016-04-20 18:43:10 -0400

    bn

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-      // This example displays a marker at the center of Australia.
-      // When the user clicks the marker, an info window opens.
-      // The maximum width of the info window is set to 200 pixels.
- function initMap() {
+// This example creates a 2-pixel-wide red polyline showing the path
+
+      function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 5,
           center: {lat: 46, lng: -355},
@@ -14,7 +13,7 @@ $(document).ready(function() {
           {lat: 51.5074, lng: 0.1278}, //London
           {lat: 50.8503, lng: 4.3517}, //Bruss
           {lat: 52.3702, lng: 4.8952}, //Amstr
-          {lat: 49.9821, lng: 7.9301}, //Rudesheim
+          {lat: 49.9929, lng: 8.2473}, //Rudesheim
           {lat: 47.0502, lng: 8.3093}, //Switz
           {lat: 48.1351, lng: 11.5820}, //Munich
           {lat: 48.2082, lng: 16.3738}, //Vienna
@@ -36,38 +35,4 @@ $(document).ready(function() {
         flightPath.setMap(map);
       }
 
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-  });
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: London,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
 }
