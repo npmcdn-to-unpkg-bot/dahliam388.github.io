ea4ab44a46c698c3fa1eb095b1866c05b28433ed
dahliam388 <dahliam@live.com>
2016-04-20 20:13:03 -0400

    fina proj lift off

--- Class-work/responsive-navbar/js/app.js
+++ Class-work/responsive-navbar/js/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
 	// console.logs the window.width
 	$(window).resize(function () {
-		console.log($(window).width())
-	})
+		console.log($(window).width());
+	});
+
+	$('#burger').click(function () {
+		$('#mobile-nav').slideToggle();
+	});
+
 })
+
+
